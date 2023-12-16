3d33f50f1ada911caeb76a38b2b8b0bfb75df746
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-12-16 22:29:56 +0530

    style: Responsive Navbar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -23,9 +23,11 @@ const Dashboard = () => {
   ];
   return (
     <div id="dashboard_page" className="bg-primary h-screen p-2">
-      <div className="flex p-8 items-center">
+      <div className="flex flex-col p-8 items-center md:flex-row md:mt-0">
         <h1 className="font-custom text-3xl mr-8">Dashboard</h1>
-        <Navbar LinkArray={navs} />
+        <div className="mt-4 md:mt-0">
+          <Navbar LinkArray={navs} />
+        </div>
       </div>
       <div className="flex mx-8 h-[80%] border-2 border-dashed border-secondary rounded-md p-6">
         <Outlet />
