7d4a1af19afd77b69696c4dcd23d577bfaf835f5
Srikanth Reddy <sriors123@gmail.com>
2023-12-17 00:10:47 +0530

    hover fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ LinkArray }) => {
         {LinkArray.map((item) => (
           <li
             key={item.id}
-            className={`font-custom px-4 py-2 text cursor-pointer font-medium text-primary border-0 rounded-3xl hover:bg-hoverColor transition-colors duration-300 ease-in-out ${
+            className={`font-custom px-4 py-2 text cursor-pointer font-medium text-primary border-0 rounded-3xl transition-colors duration-300 ease-in-out ${
               location.pathname === item.path ? 'active' : ''
             }`}
           >
