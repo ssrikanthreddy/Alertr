d72cf700c64485d904358a44a750a8153fbf25c7
Srikanth Reddy <sriors123@gmail.com>
2023-12-17 00:12:45 +0530

    hover fix #2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = ({ LinkArray }) => {
       <ul className="flex items-center gap-2 justify-around bg-tertiary border-0 rounded-3xl p-1">
         {LinkArray.map((item) => (
           <li
+            id="item"
             key={item.id}
             className={`font-custom px-4 py-2 text cursor-pointer font-medium text-primary border-0 rounded-3xl transition-colors duration-300 ease-in-out ${
               location.pathname === item.path ? 'active' : ''
