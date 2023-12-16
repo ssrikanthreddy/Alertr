eb0a78b1445f2f6fb5497a2100fd634ee36f41eb
Srikanth Reddy <sriors123@gmail.com>
2023-12-16 17:50:23 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
+    <>
     <div>Dashboard</div>
+    <div className='navbar'>
+        <div>Live Inventory</div>
+        <div>✨AI Predictive Analysis</div>
+        <div>Settings</div>
+    </div>
+    </>
   )
 }
 
